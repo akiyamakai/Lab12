277f2dd82d06c2b7c6b4656fc750293958743712
grush <grush@DESKTOP-P5A2MM5>
2024-05-06 12:50:49 +0300

    Добавьте файлы проекта.

---
+++ Lab12/Lab12.cpp
@@ -0,0 +1,44 @@
+﻿#include <iostream>
+#include <vector>
+
+using namespace std;
+
+
+pair<int, int> findMinMaxIndicesSumAndProduct(const vector<vector<int>>& matrix) {
+    int minRow = 0, minCol = 0, maxRow = 0, maxCol = 0;
+
+
+    for (int i = 0; i < matrix.size(); ++i) {
+        for (int j = 0; j < matrix[i].size(); ++j) {
+            if (matrix[i][j] < matrix[minRow][minCol]) {
+                minRow = i;
+                minCol = j;
+            }
+            if (matrix[i][j] > matrix[maxRow][maxCol]) {
+                maxRow = i;
+                maxCol = j;
+            }
+        }
+    }
+
+
+    return make_pair(minRow + minCol + maxRow + maxCol, minRow * minCol * maxRow * maxCol);
+}
+
+int main() {
+
+    vector<vector<int>> matrix = {
+        {1, 2, 3},
+        {4, 5, 6},
+        {7, 8, 9}
+    };
+
+
+    pair<int, int> result = findMinMaxIndicesSumAndProduct(matrix);
+
+
+    cout << "Summa index: " << result.first << endl;
+    cout << "Dobutok index: " << result.second << endl;
+
+    return 0;
+}
